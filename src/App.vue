b5a2797f1e2d4f8526cbefdf5fedf900a1f680fc
ShadePrima <aleksandrag1984@gmail.com>
2022-04-29 15:42:31 +0300

    add sorted

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
     <div class='app'>
         <h1>Posting list</h1>
-        <my-button
-        @click='showDialog'
-        style='margin: 5px 0'
-        >
-        Create post
-        </my-button>
+        <div class='app__btns'>
+            <my-button
+            @click='showDialog'
+            >
+            Create post
+            </my-button>
+            <my-select
+                v-model='selectedSort'
+                :options='sortOption'
+            />
+        </div> 
         <my-dialog v-model:show='dialogVisible'>
             <post-form
             @create='createPost'
@@ -29,21 +34,28 @@
 import PostForm from '@/components/PostForm.vue'
 import PostList from '@/components/PostList.vue'
 import MyDialog from './components/UI/MyDialog.vue'
+
 import axios from 'axios'
+import MySelect from './components/UI/MySelect.vue'
 
     export default {
         components: {
-            PostForm, PostList,
-                MyDialog
+            PostForm, PostList, MyDialog,
+                MySelect
             },
         data () {
             return {
                 posts: [],
                 dialogVisible: false,
                 isPostsLoading: false,
+                selectedSort: '',
+                sortOption: [
+                    {value: 'title', name: 'By name'},
+                    {value: 'body', name: 'By content'}
+                ]
             }
         }, 
-        methods: {
+        methods: { 
             createPost(post) {
                 this.posts.push(post)
                 this.dialogVisible = false
@@ -68,10 +80,34 @@ import axios from 'axios'
         },
         mounted() {
             this.fetchPosts()
+        },
+        
+          
+        watch: {
+            selectedSort(newValue) {
+                this.posts.sort((post1, post2) => {
+                    return post1[newValue]?.localeCompare(post2[newValue])
+                }) 
+            }
+          
         }
     }
 </script>
 
-<style>
+<style> 
+* {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+}
 
+.app {
+    padding: 20px;
+}
+
+.app__btns {
+    margin: 15px 0;
+    display: flex;
+    justify-content: space-between;
+}
 </style>
